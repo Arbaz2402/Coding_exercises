911db1f6608dee2496a9e65fa5c596a17da36963
Arbaz2402 <121999150+Arbaz2402@users.noreply.github.com>
2023-11-18 14:00:07 +0530

    Update Smart_Home_System.cpp

--- Smart_Home_System.cpp
+++ Smart_Home_System.cpp
@@ -1,99 +1,81 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <ctime>
 
 // Base class for smart devices
 class Device {
-public:
-    Device(int device_id, const std::string& device_type) : device_id(device_id), device_type(device_type) {}
-
-    virtual void turnOn() = 0;
-    virtual void turnOff() = 0;
-    virtual std::string getStatus() const = 0;
-
 protected:
-    int device_id;
-    std::string device_type;
+    int deviceID;
+    std::string deviceType;
+
+public:
+    Device(int deviceID, std::string deviceType) : deviceID(deviceID), deviceType(deviceType) {}
+
+    int getDeviceID() const {
+        return deviceID;
+    }
+
+    virtual std::string getStatus() = 0;
 };
 
 // Light class, a subclass of Device
 class Light : public Device {
-public:
-    Light(int device_id) : Device(device_id, "Light"), status("off") {}
+private:
+    std::string status;
 
-    void turnOn() override {
+public:
+    Light(int deviceID) : Device(deviceID, "Light"), status("off") {}
+
+    void turnOn() {
         status = "on";
     }
 
-    void turnOff() override {
+    void turnOff() {
         status = "off";
     }
 
-    std::string getStatus() const override {
-        return "Light " + std::to_string(device_id) + " is " + status + ".";
+    std::string getStatus() override {
+        return "Light " + std::to_string(getDeviceID()) + " is " + status + ".";
     }
-
-private:
-    std::string status;
 };
 
 // Thermostat class, a subclass of Device
 class Thermostat : public Device {
-public:
-    Thermostat(int device_id) : Device(device_id, "Thermostat"), temperature(70) {}
-
-    void turnOn() override {
-        // Implement turnOn logic for Thermostat if needed
-    }
-
-    void turnOff() override {
-        // Implement turnOff logic for Thermostat if needed
-    }
-
-    std::string getStatus() const override {
-        return "Thermostat is set to " + std::to_string(temperature) + " degrees.";
-    }
-
 private:
     int temperature;
+
+public:
+    Thermostat(int deviceID) : Device(deviceID, "Thermostat"), temperature(70) {}
+
+    std::string getStatus() override {
+        return "Thermostat is set to " + std::to_string(temperature) + " degrees.";
+    }
 };
 
 // DoorLock class, a subclass of Device
 class DoorLock : public Device {
-public:
-    DoorLock(int device_id) : Device(device_id, "DoorLock"), status("locked") {}
-
-    void turnOn() override {
-        // Implement turnOn logic for DoorLock if needed
-    }
-
-    void turnOff() override {
-        // Implement turnOff logic for DoorLock if needed
-    }
-
-    std::string getStatus() const override {
-        return "Door is " + status + ".";
-    }
-
 private:
     std::string status;
+
+public:
+    DoorLock(int deviceID) : Device(deviceID, "DoorLock"), status("locked") {}
+
+    std::string getStatus() override {
+        return "Door is " + status + ".";
+    }
 };
 
 // Factory for creating devices
 class DeviceFactory {
 public:
-    static Device* createDevice(int device_id, const std::string& device_type) {
-        if (device_type == "Light") {
-            return new Light(device_id);
-        }
-        else if (device_type == "Thermostat") {
-            return new Thermostat(device_id);
-        }
-        else if (device_type == "DoorLock") {
-            return new DoorLock(device_id);
-        }
-        else {
+    static Device* createDevice(int deviceID, std::string deviceType) {
+        if (deviceType == "Light") {
+            return new Light(deviceID);
+        } else if (deviceType == "Thermostat") {
+            return new Thermostat(deviceID);
+        } else if (deviceType == "DoorLock") {
+            return new DoorLock(deviceID);
+        } else {
             return nullptr; // Handle unknown device types
         }
     }
@@ -101,37 +83,41 @@ public:
 
 // Proxy class for controlling devices
 class DeviceController {
+private:
+    std::vector<Device*> devices;
+
 public:
     void addDevice(Device* device) {
         devices.push_back(device);
     }
 
-    void turnOn(int device_id) {
-        for (auto& device : devices) {
-            if (device->getStatus().find(" " + std::to_string(device_id) + " ") != std::string::npos) {
-                device->turnOn();
+    void turnOn(int deviceID) {
+        for (Device* device : devices) {
+            if (device->getDeviceID() == deviceID) {
+                if (Light* lightDevice = dynamic_cast<Light*>(device)) {
+                    lightDevice->turnOn();
+                }
             }
         }
     }
 
-    void turnOff(int device_id) {
-        for (auto& device : devices) {
-            if (device->getStatus().find(" " + std::to_string(device_id) + " ") != std::string::npos) {
-                device->turnOff();
+    void turnOff(int deviceID) {
+        for (Device* device : devices) {
+            if (device->getDeviceID() == deviceID) {
+                if (Light* lightDevice = dynamic_cast<Light*>(device)) {
+                    lightDevice->turnOff();
+                }
             }
         }
     }
 
-    std::string getStatusReport() const {
-        std::string report;
-        for (const auto& device : devices) {
+    std::string getStatusReport() {
+        std::string report = "";
+        for (Device* device : devices) {
             report += device->getStatus() + " ";
         }
         return report;
     }
-
-private:
-    std::vector<Device*> devices;
 };
 
 int main() {
@@ -139,12 +125,9 @@ int main() {
     controller.addDevice(DeviceFactory::createDevice(1, "Light"));
     controller.addDevice(DeviceFactory::createDevice(2, "Thermostat"));
     controller.addDevice(DeviceFactory::createDevice(3, "DoorLock"));
-
     controller.turnOn(1);
     controller.turnOn(2);
     controller.turnOn(3);
-
     std::cout << controller.getStatusReport() << std::endl;
-
     return 0;
 }
